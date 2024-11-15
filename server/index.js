f36fabc27e3fa07b1cb0cfde62ca33ee226225a1
Vidit Srivastava <148772849+viditsri184@users.noreply.github.com>
2024-11-15 17:50:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,11 @@ const swaggerDocs = swaggerJsdoc(swaggerOptions);
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+    origin: 'http://localhost:5173', // Replace with your frontend URL in production
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+));
 app.use('/api', router);
 
 const port = process.env.PORT || 8080;
